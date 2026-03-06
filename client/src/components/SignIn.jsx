33b0086d48eb08a071e54cc674ba1c129e995e7c
Nicholas Croulet <tug61145@temple.edu>
2026-03-06 00:01:41 -0500

    modified css of sign in

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -21,20 +21,44 @@ export default function SignIn() {
 
   };
   return (
-    <section id="contact">
-        <h1>Sign In:</h1>
-        <br></br>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={form.email} onChange={handleChange} required></input>
-          <br></br>
-          <label htmlFor="password">Password:</label>
-          <input type="text" id="password" name="password" value={form.password} onChange={handleChange} required></input>
-          <br></br>
-          <input type="submit" value="Submit" onClick={buttonAction}></input>
-        </form>
-        <h3>Don't Have an Account?</h3>
-        <button>Create</button>      
-    </section>
+      <div class="">
+        <header class="bg-blue-500 p-4 font-mono text-white">Lawbot</header>
+        <div class="flex flex-col md:flex-row flex-grow items-center justify-center">
+          <main class="flex-grow p-4 order-2 md:order-1">
+            <div class="w-full max-w-xs">
+              <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                  <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    Username
+                  </label>
+                  <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" onChange={handleChange} required></input>
+                </div>
+                <div class="mb-6">
+                  <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                  </label>
+                  <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" onChange={handleChange} required></input>
+                  <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
+                <div class="flex items-center justify-between">
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={buttonAction}>
+                    Sign In
+                  </button>
+                  <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                    Forgot Password?
+                  </a>
+                </div>
+              </form>
+              <p class="text-center text-gray-500 text-xs">
+                &copy;2026 Lawbot Co. All rights reserved.
+              </p>
+            </div>
+          </main>
+          <aside class="w-full md:w-90 p-4 bg-white-200 order-1 md:order-2">
+            <img src='client\public\logo512.png'></img>
+          </aside>
+        </div>
+      </div>
+
   )
 };
