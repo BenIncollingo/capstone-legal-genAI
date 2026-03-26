95b4a95232eb56b90c7539f469468dfe83113de4
ben-incollingo <bincollingo@outlook.com>
2026-03-26 10:19:32 -0400

    config changes

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+export const BACKEND_API_BASE_URL = process.env.REACT_APP_BACKEND_API_URL || "http://localhost:8080/api";
